e01cdd0195cee52ccd93e3ab8fc35839cc1de2a1
Leandro <leandro.feitoza8018@gmail.com>
2021-03-07 22:02:49 -0300

    finalizado Dropzone basico

--- src/components/Upload/index.js
+++ src/components/Upload/index.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import Dropzone from 'react-dropzone'
+import Dropzone from '../Dropzone'
 import './style.css'
 
 class Update extends Component {
@@ -7,21 +7,10 @@ class Update extends Component {
     files: [],
   }
 
-  handleUpload = files => {
-    console.log(files)
-  }
-
   render() {
     return (
       <>
-        <Dropzone accept='image/*' onDropAccepted={this.handleUpload} >
-          { ({ getRootProps, getInputProps, isDragActive, isDragReject }) => (
-           <div className='Drop-container' {...getRootProps()} >
-            <input {...getInputProps()} />
-            <p>Arraste e solte. Ou clique para selecionar arquivos.</p>
-           </div>
-          ) }
-        </Dropzone>
+        <Dropzone />
       </>
     )
   }
